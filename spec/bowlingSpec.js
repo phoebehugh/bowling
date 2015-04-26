07ae014efd519ff8fbc63ce87983c9e78ab65407
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-26 12:10:24 +0100

    Refacotrs with a rollMany function

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,20 +5,22 @@ describe('Bowling game', function() {
     game = new Game();
   });
 
+  var rollMany = function(pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+        game.roll(pins);
+    };  
+  };
+
   describe('Gutter game', function() {
     it('should score 0 when no rolls hit any pins', function() {
-      for (var i = 0; i < 20; i++) {
-          game.roll(0);
-        };  
+      rollMany(0, 20);
       expect(game.score()).toBe(0);
     });
   });
 
   describe('Game when all rolls hit only 1 pin', function() {
     it('should score 20 when 1 pin hit every roll', function() {
-      for (var i = 0; i < 20; i++) {
-          game.roll(1);
-        };
+      rollMany(1, 20);
       expect(game.score()).toBe(20);
     });
   });
