f0b9ba814dac65f83eac03de5be81b18929ec5ed
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-26 12:04:23 +0100

    Scores 20 when 1 is hit every roll

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -7,10 +7,19 @@ describe('Bowling game', function() {
 
   describe('Gutter game', function() {
     it('should score 0 when no rolls hit any pins', function() {
-      for (i = 0; i < 20; i++) {
-          game.roll(0)
-          expect(game.score).toEqual(0)
+      for (var i = 0; i < 20; i++) {
+          game.roll(0);
         };  
+      expect(game.score()).toBe(0);
+    });
+  });
+
+  describe('Game when all rolls hit only 1 pin', function() {
+    it('should score 20 when 1 pin hit every roll', function() {
+      for (var i = 0; i < 20; i++) {
+          game.roll(1);
+        };
+      expect(game.score()).toBe(20);
     });
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,11 +1,15 @@
 var Game = function() {
-  this.score = 0
+  this.rolls = [];
 };
 
 Game.prototype.roll = function(pins) {
-  this.score += pins
+  this.rolls.push(pins);
 };
 
-Game.prototype.score = function(pins) {
-  return -1
+Game.prototype.score = function() {
+   var result = 0;
+   for (var i = 0; i < 20; i++) {
+    result += this.rolls[i];
+   }
+   return result;
 };
